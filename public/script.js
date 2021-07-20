cc2006e299e735794e8cea874a5e4382a6bd7228
Yuvraj Chahal <73404413+yuvchal@users.noreply.github.com>
2021-07-19 23:23:08 -0400

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,52 @@
+"use strict";
+
+//variable setting
+const btnLogin = document.querySelector(".login");
+const btnView = document.querySelector(".view");
+const password = document.querySelector(".login__input--password");
+const username = document.querySelector(".login__input--user");
+
+const passwordView = function () {
+  if (password.type === "text") password.type = "password";
+  else password.type = "text";
+};
+
+btnView.addEventListener("click", passwordView);
+
+$(function () {
+  $(".login").on("click", function () {
+    if (password.value.length < 8) {
+      alert("not enough characters in the inputted password");
+    } else {
+      //API CALL
+      const loginAttempt = {
+        username: $(".login__input--user").val(),
+        password: $(".login__input--password").val(),
+      };
+      $.ajax({
+        type: "POST",
+        url: "/auth",
+        data: loginAttempt,
+        success: function (correct) {
+          if (correct === "true") {
+            // clearing the web page
+            $(".container-fluid").fadeToggle(1000);
+            $(".desc").fadeToggle(1000);
+            $(".desc2").fadeToggle(1000);
+            setTimeout(() => {
+              document.querySelector(".desc").textContent =
+                "This page will display all employees by name and ID along with their skills & certifications";
+              $(".desc").fadeToggle(1000);
+              document.querySelector(
+                ".desc2"
+              ).textContent = `Click "List of users" to begin`;
+              $(".desc2").fadeToggle(2000);
+            }, 1000);
+          } else {
+            alert("login unsuccsesful");
+          }
+        },
+      });
+    }
+  });
+});
---
+++ src/server.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const path = require('path');
+const fs = require("fs");
+
+// ignore request for FavIcon. so there is no error in browser
+const ignoreFavicon = (req, res, next) => {
+    if (req.originalUrl.includes('favicon.ico')) {
+        res.status(204).end();
+    }
+    next();
+};
+
+// fn to create express server
+const create = async () => {
+
+    // server
+    const app = express();
+
+    // configure nonFeature
+    // app.use(ignoreFavicon);
+    app.use(express.static("public/"));
+    app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+    // root route - serve static file
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/client.html'));
+    });
+
+    console.log("directory: ",__dirname);
+app.get("/listUsers", function (req, res) {
+  fs.readFile(__dirname + "/users.json", "utf8", function (err, data) {
+    console.log("data: ", data);
+    res.end(data);
+  });
+});
+
+app.post("/auth", function (req, res) {
+    console.log("auth called");
+    console.log(req.body);
+    console.log("request:", req.body.username, req.body.password, req.body.id);
+    fs.readFile(__dirname + "/users.json", "utf8", function (err, data) {
+      const users = JSON.parse(data);
+      console.log("users:", users);
+      let beenReturned = false;
+      for (let i = 0; i < users.length; i++) {
+        const tempUser = users[i];
+        console.log(tempUser);
+        if (
+          tempUser.username == req.body.username &&
+          tempUser.password == req.body.password
+        ) {
+          console.log(true);
+          res.send("true");
+          beenReturned = true;
+          break;
+        }
+      }
+      if (!beenReturned) {
+        console.log(false);
+        res.send("false");
+      }
+    });
+  });
+  
+
+
+    // Error handler
+    /* eslint-disable no-unused-vars */
+    app.use((err, req, res, next) => {
+        console.error(err.stack);
+        res.status(500).send('Something broke!');
+    });
+    return app;
+};
+
+module.exports = {
+    create
+};
