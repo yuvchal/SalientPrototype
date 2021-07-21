c5ffc156e5024c2a809510abb1542a91e63bac89
Yuvraj Chahal <73404413+yuvchal@users.noreply.github.com>
2021-07-21 18:22:59 -0400

    Add files via upload

--- public/script2.js
+++ public/script2.js
@@ -3,6 +3,9 @@
 $('.container-fluid').hide();
 $('.login').hide();
 $('.info').hide();
+$('.table').hide();
+$('.mydatatable').hide();
+$('#sorting').hide();
 setTimeout(() => {
   $('.container-fluid').fadeToggle(1000);
   $('.login').fadeToggle(1000);
@@ -10,9 +13,10 @@ setTimeout(() => {
 
 $(function () {
   $('.login').on('click', function () {
-    $('.login').hide(1000);
+    $('.login').fadeToggle(1000);
     $('.info').show(1000);
+    $('.mydatatable').DataTable();
+    $('.mydatatable').show(1000);
+    $('#sorting').fadeToggle(1000);
   });
 });
-
-// $(function () {});
